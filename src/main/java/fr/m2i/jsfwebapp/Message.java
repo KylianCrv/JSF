372fa7cec947c49aa8ab41f484e02eed630167b6
KylianCrv <kylian.crv@hotmail.com>
2022-07-29 12:29:55 +0200

    helloworld.java + message.java

---
+++ src/main/java/fr/m2i/jsfwebapp/Message.java
@@ -0,0 +1,21 @@
+package fr.m2i.jsfwebapp;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "message", eager = true)
+public class Message {
+
+    private String message;
+
+    public Message() {
+        this.message = "Le message depuis le Bean Message";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
